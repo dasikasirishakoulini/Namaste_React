d7d30641efdc778d43fab8f457bc395d73721900
sirisha dasika <sirishadasika@sirishas-MacBook-Air.local>
2025-01-30 18:33:00 +0530

    EPISODE-1

---
+++ App.js
@@ -0,0 +1,17 @@
+const heading = React.createElement(
+    "div",
+    {id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"i m an h1 tag of child1"),
+        React.createElement("h2",{},"i am an h2 tag of child1")
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"i m an h1 tag of child2"),
+        React.createElement("h2",{},"i am an h2 tag of child2")
+    ]),
+]);
+
+    console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+        root.render(heading);
