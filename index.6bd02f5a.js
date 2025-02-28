3bd33821e2eb0e959c092270204ddc934ab3666e
sirisha dasika <sirishadasika@sirishas-MacBook-Air.local>
2025-02-28 21:12:54 +0530

    Updates

---
+++ index.6bd02f5a.js
@@ -0,0 +1,21 @@
+const heading = React.createElement("div", {
+    id: "parent"
+}, [
+    React.createElement("div", {
+        id: "child1"
+    }, [
+        React.createElement("h1", {}, "i m an h1 tag of child1"),
+        React.createElement("h2", {}, "i am an h2 tag of child1")
+    ]),
+    React.createElement("div", {
+        id: "child2"
+    }, [
+        React.createElement("h1", {}, "i m an h1 tag of child2"),
+        React.createElement("h2", {}, "i am an h2 tag of child2")
+    ])
+]);
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
